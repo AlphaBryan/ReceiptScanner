ad8d28abdd3f07ba24733ba6a1b118a4af7d9ab3
alphabryan <90648570+AlphaBryan@users.noreply.github.com>
2023-02-12 00:34:33 -0500

    Base version

--- scanningMotors/scanningMotors.ts
+++ scanningMotors/scanningMotors.ts
@@ -7,7 +7,7 @@ import { gcpPictureScanning } from "./gcpTextDetectort";
 import { init } from "./lineSegmentation/test1/lineSegmentationGpcVision";
 // @ts-ignore
 import { initLineSegmentation } from "./lineSegmentation/test2/lineSegmentationGpcVision";
-
+import * as FileSystem from "expo-file-system";
 
 export async function scanningMotors(file: string) {
   try {
@@ -15,10 +15,20 @@ export async function scanningMotors(file: string) {
     // @ts-ignore
     const scanResult = await gcpPictureScanning(file.base64);
     console.log("Analyze Done.");
-    let segmentedResult = initLineSegmentation(scanResult.responses[0]);
+    let segmentedResult = init(scanResult.responses[0]);
     const fullTextAnnotation = scanResult.responses[0].fullTextAnnotation;
     const text = fullTextAnnotation?.text || "";
-    console.log("result: ", segmentedResult);
+    const textAnnotations = scanResult.responses[0].textAnnotations;
+    //JSON file
+
+    console.log("resultats: ", JSON.stringify(fullTextAnnotation, null, 2));
+    // creating a file to save the result
+    // const fileUri = FileSystem.documentDirectory + "result.txt";
+    // await FileSystem.writeAsStringAsync(fileUri, segmentedResult);
+
+    // // get the file content
+    // const fileContent = await FileSystem.readAsStringAsync(fileUri);
+    // console.log("fileContent: ", fileContent);
     return text;
   } catch (error) {
     console.log("Scanning motors error: ", error);

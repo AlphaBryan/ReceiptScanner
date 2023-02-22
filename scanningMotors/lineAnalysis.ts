0a2b802364a078eb28af790a2db8d8ec0eb21cc6
alphabryan <90648570+AlphaBryan@users.noreply.github.com>
2023-02-22 13:50:20 -0500

    One version

--- scanningMotors/lineAnalysis.ts
+++ scanningMotors/lineAnalysis.ts
@@ -5,74 +5,52 @@ import {
 } from "./coordinateAnalysis";
 
 export function lineAnalysis(OcrResult: Array<any>): any {
-  const lines: Line[] = [];
-  let currentLine: Line | null = null;
-
-  // sort the OcrResult by x coordinate
-  
-
-
-  // Iterate through each element in OcrResult
-  // for (const element,index of OcrResult) {
+  const lines: { [key: string]: Line } = {};
   OcrResult.forEach((element, index) => {
-    // if (index > 0) {
-      // console.log("index: ", index);
-      // Get the center point of the bounding box of the current element
+    if (index > 0) {
       const elementCenter = getSquareCenter(element.boundingPoly.vertices);
-
-      // If currentLine is null or the current element is not on the same line as the previous element
-      if (
-        currentLine === null ||
-        !isOnTheSameLine(elementCenter, currentLine.coords)
-      ) {
-        // Create a new line object
-        currentLine = { coords: elementCenter, text: element.description };
-        lines.push(currentLine);
+      const previousElementCenter = getSquareCenter(
+        OcrResult[index - 1].boundingPoly.vertices
+      );
+      if (isOnTheSameLine(elementCenter, previousElementCenter)) {
+        var count = Object.keys(lines).length;
+        lines[count - 1].text =
+          lines[count - 1].text + " " + element.description;
+        // faire la moyenne des coordonnées x et y
+        lines[count - 1].coords.x =
+          (lines[count - 1].coords.x + elementCenter.x) / 2;
+        lines[count - 1].coords.y =
+          (lines[count - 1].coords.y + elementCenter.y) / 2;
       } else {
-        // Concatenate the current element's description to the previous line's text
-        currentLine.text += ` ${element.description}`;
-        // Calculate the average coordinates of the current line
-        currentLine.coords.x = (currentLine.coords.x + elementCenter.x) / 2;
-        currentLine.coords.y = (currentLine.coords.y + elementCenter.y) / 2;
-      }
-    // }
-  });
-  OcrResult.sort((a, b) => {
-    const aCenter = getSquareCenter(a.boundingPoly.vertices);
-    const bCenter = getSquareCenter(b.boundingPoly.vertices);
-    return aCenter.x - bCenter.x;
-  });
-
-  // Group lines by approximate x-coordinate
-  const xTolerance = 20; // adjust this value to suit your needs
-  const lineGroups: Map<number, Line[]> = new Map();
-  for (const line of lines) {
-    const groupKey = Math.round(line.coords.x / xTolerance);
-    if (!lineGroups.has(groupKey)) {
-      lineGroups.set(groupKey, []);
-    }
-    lineGroups.get(groupKey)!.push(line);
-  }
-
-  // Merge lines within each group
-  const mergedLines: Line[] = [];
-  for (const group of lineGroups.values()) {
-    group.sort((a, b) => a.coords.x - b.coords.x);
-    let mergedLine: Line = group[0];
-    for (let i = 1; i < group.length; i++) {
-      const line = group[i];
-      if (isOnTheSameLine(line.coords, mergedLine.coords)) {
-        mergedLine.text += ` ${line.text}`;
-        mergedLine.coords.x = (mergedLine.coords.x + line.coords.x) / 2;
-        mergedLine.coords.y = (mergedLine.coords.y + line.coords.y) / 2;
-      } else {
-        mergedLines.push(mergedLine);
-        mergedLine = line;
+        var count = Object.keys(lines).length;
+        const line: Line = {
+          coords: elementCenter,
+          text: element.description,
+        };
+        lines[count] = line;
       }
     }
-    mergedLines.push(mergedLine);
-  }
+  });
+  Object.entries(lines).forEach(([key, value]) => {
+    Object.entries(lines).forEach(([key2, value2]) => {
+      if (key !== key2) {
+        if (isOnTheSameLine(value.coords, value2.coords)) {
+          if (lines[key]) {
+            lines[key].coords.x =
+              (lines[key].coords.x + lines[key2].coords.x) / 2;
+            lines[key].coords.y =
+              (lines[key].coords.y + lines[key2].coords.y) / 2;
+            lines[key].text = lines[key]?.text + " " + lines[key2]?.text;
+            delete lines[key2];
+          }
+        }
+      }
+    });
+  });
 
-  console.log("mergedLines: ", JSON.stringify(mergedLines, null, 4));
-  return mergedLines;
+  // Ordonner les lignes par ordre croissant de coordonnées x
+  const linesArray = Object.values(lines);
+  linesArray.sort((a, b) => a.coords.x - b.coords.x);
+  console.log("linesArray: ", JSON.stringify(linesArray, null, 4));
+  return linesArray;
 }
